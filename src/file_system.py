8ee19707b94cfdd6c464aa37065c9f0139724205
AveWycc220 <mik16748@gmail.com>
2020-10-25 17:57:42 +0300

    feat (src/file_system): add method for crate categories folders

--- src/file_system.py
+++ src/file_system.py
@@ -9,11 +9,19 @@ class FileSystem:
     """ Class for working with Windows File System """
     @staticmethod
     def create_parser_folder():
+        """ If parser folder doesnt exist create new """
         if 'data_parser' not in os.listdir(path=f'{THIS_FOLDER}\\..\\'):
             os.mkdir(path=f'{THIS_FOLDER}\\..\\data_parser')
 
     @staticmethod
     def create_folder(type: str, request=None):
+        """ Create folders depending on types """
         if type == 'request':
             if f'{request}' not in os.listdir(path=f'{THIS_FOLDER}\\..\\data_parser\\') and request:
                 os.mkdir(path=f'{THIS_FOLDER}\\..\\data_parser\\{request}')
+
+    @staticmethod
+    def create_categoies_folders(request, categories):
+        """ Create Categories Folders"""
+        for elem in categories:
+            os.mkdir(path=f'{THIS_FOLDER}\\..\\data_parser\\{request}\\{elem}')

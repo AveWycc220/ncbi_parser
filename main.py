c545ef33c9edaf54034ca5b0053f3e380c4262c5
AveWycc220 <mik16748@gmail.com>
2020-10-28 13:30:13 +0300

    feat (main): add console interface for users

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from src.file_system import FileSystem
 from src.parser import Parser
 
-Parser.get_request('Rabies virus')
+print('Type a request')
+request = input('>> ')
+print('1 -- Literature\n   1.1 -- Books\n   1.2 -- MeSH\n   '
+      '1.3 -- NLM Catalog\n   1.4 -- PubMed\n   1.5 -- PubMed Central')
+catalog = input('>> ')
+Parser.get_request(request, catalog)

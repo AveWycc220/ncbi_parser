dec67afd2cf591a46965fa429c053c38a5c59cea
AveWycc220 <mik16748@gmail.com>
2020-11-03 18:17:43 +0300

    feat (src/parser.py): add method for GEO Profiles

--- src/parser.py
+++ src/parser.py
@@ -62,7 +62,8 @@ class Parser:
             Parser.__pmc(request)
         elif catalog == '2':
             #Parser.__gene(request)
-            Parser.__gds(request)
+            #Parser.__gds(request)
+            Parser.__geo(request)
 
     @staticmethod
     def __replace_elem_in_request(request):
@@ -507,4 +508,67 @@ class Parser:
             except WebDriverException:
                 driver.close()
 
-
+    @staticmethod
+    def __geo(request):
+        string_request = request
+        request = Parser.__replace_elem_in_request(request)
+        print(request)
+        content = requests.get(f'https://www.ncbi.nlm.nih.gov/geoprofiles/?term={request}').content.decode('utf-8')
+        options = Options()
+        options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(executable_path=FileSystem.get_driver(), options=options)
+        driver.get(url=f'https://www.ncbi.nlm.nih.gov/geoprofiles/?term={request}')
+        soup = BeautifulSoup(content, 'html.parser')
+        content_list = []
+        genes_url_list = []
+        if not soup.findAll(True, {'class': 'warn'}):
+            content_list.append(driver.page_source)
+            try:
+                next = driver.find_element_by_class_name('next')
+                while 'inactive' not in next.get_attribute('class'):
+                    next.click()
+                    content_list.append(driver.page_source)
+                    next = driver.find_element_by_class_name('next')
+            except NoSuchElementException:
+                pass
+            for i in range(0, len(content_list)):
+                soup = BeautifulSoup(content_list[i], 'html.parser')
+                titles_list = soup.findAll(True, {"class": "title"})
+                for j in range(0, len(titles_list)):
+                    genes_url_list.append(titles_list[j].a['href'])
+            for i in range(0, len(genes_url_list)):
+                driver.get(url=f'https://www.ncbi.nlm.nih.gov{genes_url_list[i]}')
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                full_title = soup.findAll(True, {"class": "title"})[0].text
+                full_title = Parser.__replace_elem_for_windows(full_title[14:240])
+                full_title = Parser.__replace_elem_for_windows(full_title)
+                print(full_title)
+                if not FileSystem.is_exist(f'{full_title}.html',
+                                        f'{FileSystem.get_directory()}data_parser\\'
+                                        f'{string_request}\\Genes\\GEO Profiles\\'):
+                    pyautogui.hotkey('ctrl', 's')
+                    time.sleep(TIME_SLEEP * 2)
+                    pyautogui.typewrite(
+                        f'{FileSystem.get_directory()}data_parser\\'
+                        f'{string_request}\\Genes\\GEO Profiles\\{full_title}.html')
+                    time.sleep(TIME_SLEEP / 2)
+                    pyautogui.hotkey('enter')
+                    time.sleep(TIME_SLEEP)
+                profile = driver.find_element_by_class_name('rprt')
+                profile = profile.find_element_by_tag_name('a')
+                profile.click()
+                if not FileSystem.is_exist(f'profile_{full_title}.html',
+                                        f'{FileSystem.get_directory()}data_parser\\'
+                                        f'{string_request}\\Genes\\GEO Profiles\\'):
+                    pyautogui.hotkey('ctrl', 's')
+                    time.sleep(TIME_SLEEP * 2)
+                    pyautogui.typewrite(
+                        f'{FileSystem.get_directory()}data_parser\\'
+                        f'{string_request}\\Genes\\GEO Profiles\\profile_{full_title}.html')
+                    time.sleep(TIME_SLEEP / 2)
+                    pyautogui.hotkey('enter')
+                    time.sleep(TIME_SLEEP)
+            try:
+                raise WebDriverException
+            except WebDriverException:
+                driver.close()
